f5666741c1836d29a9485fd6a2803318a28d5649
Igor Yakubov <igor.yakubov@telus.com>
2021-07-14 23:06:11 -0700

    use index instead element

--- remote-state/Layer1-Network/main.tf
+++ remote-state/Layer1-Network/main.tf
@@ -76,5 +76,5 @@ resource "aws_route_table" "public_subnets" {
 resource "aws_route_table_association" "public_routes" {
   count          = length(aws_subnet.public_subnets[*].id)
   route_table_id = aws_route_table.public_subnets.id
-  subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
+  subnet_id      = aws_subnet.public_subnets[count.index].id
 }
